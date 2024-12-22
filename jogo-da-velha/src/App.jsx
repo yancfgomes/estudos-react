c8177774a7ca49a862a00c241ef2130d6039fc14
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2024-12-21 23:20:27 -0400

    style: apply consistent code formatting and add Prettier configuration

--- jogo-da-velha/src/App.jsx
+++ jogo-da-velha/src/App.jsx
@@ -1,40 +1,36 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-function Square(){
-  const[value, setValue] = useState(null);
+function Square() {
+    const [value, setValue] = useState(null)
 
-
-  function handleClick(){
-    setValue('X');
-  }
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
-      >{value}
-    </button>);
+    function handleClick() {
+        setValue('X')
+    }
+    return (
+        <button className="square" onClick={handleClick}>
+            {value}
+        </button>
+    )
 }
 
-
 export default function Board() {
-  return (
-    <>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-
-    </>
-  );
+    return (
+        <>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </>
+    )
 }
