2f1554f4e1befc0b55837bbf077c4849f41acc5d
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2024-12-21 22:22:16 -0400

    refactor: clean up code formatting and add EditorConfig

--- jogo-da-velha/src/App.jsx
+++ jogo-da-velha/src/App.jsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 function Square(){
   const[value, setValue] = useState(null);
 
-  
+
   function handleClick(){
     setValue('X');
   }
   return (
-    <button 
+    <button
       className="square"
       onClick={handleClick}
       >{value}
@@ -34,7 +34,7 @@ export default function Board() {
         <Square />
         <Square />
       </div>
-      
+
     </>
   );
 }
